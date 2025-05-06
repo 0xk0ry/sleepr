b838336ce4c09d8e96eb3b18d395b4b6eba9c24e
kory <phamtruc.main@gmail.com>
2025-05-06 19:17:50 +0700

    feat: implement reservations module with DTOs, controller, service, repository, and schema

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '@app/common';
-import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { ReservationsModule } from './reservations/reservations.module';
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule } from '@app/common';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
 
 @Module({
-  imports: [DatabaseModule, ReservationsModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: ReservationDocument.name,
+        schema: ReservationSchema,
+      },
+    ]),
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
-export class AppModule {}
+export class ReservationsModule {}
